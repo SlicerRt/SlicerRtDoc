a7359598a5f36f58ba502c140be85114ac94b8c1
pinter <pinter@25a7314f-c4c8-4314-9936-a25f8480a772>
2013-04-03 21:40:32 +0000

    Re #117: Added comments to the center of masses python example loading script
    git-svn-id: https://subversion.assembla.com/svn/slicerrt/trunk/SlicerRt/doc@734 25a7314f-c4c8-4314-9936-a25f8480a772

--- samples/PythonScripts/getTranslationOfCentersOfMasses_Load.py
+++ samples/PythonScripts/getTranslationOfCentersOfMasses_Load.py
@@ -1,11 +1,18 @@
-# Copy here (or to any directory included in PYTHONPATH):
+# In order to run this directly from Slicer, copy here (or to any directory included in PYTHONPATH):
 #   c:\devel\Slicer_Trunk_R64-bin\Slicer-build\lib\Slicer-4.1\qt-loadable-modules\Python
 
+# Get two volumes from the MRML scene (the first two)
 v1 = slicer.mrmlScene.GetNodeByID('vtkMRMLScalarVolumeNode1')
+v2 = slicer.mrmlScene.GetNodeByID('vtkMRMLScalarVolumeNode2')
 
+# Import the algorithm script
 import getTranslationOfCentersOfMasses
 
+# Call getCenterOfMass on the first volume
 #getTranslationOfCentersOfMasses.getCenterOfMass(v1)
+
+# Call getTranslationOfCentersOfMasses on the two volumes (displacement of the two centers)
 getTranslationOfCentersOfMasses.getTranslationOfCentersOfMasses(v1, v2)
 
+# Reload algorithm script (in case it has been changed)
 #getTranslationOfCentersOfMasses = reload(getTranslationOfCentersOfMasses)
