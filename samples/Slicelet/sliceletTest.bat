d4b7c77126be11d63fe788e184eac9ec01ed4cfb
pinter <pinter@25a7314f-c4c8-4314-9936-a25f8480a772>
2014-01-21 16:22:04 +0000

    Re #54: Added BatchContourConversion script as a simple sample for batch processing python scripts; Moved samples folder from doc to be bound more easily
    git-svn-id: https://subversion.assembla.com/svn/slicerrt/trunk/SlicerRt/doc@1453 25a7314f-c4c8-4314-9936-a25f8480a772

--- samples/Slicelet/sliceletTest.bat
+++
@@ -1,4 +0,0 @@
-rem set SLICER_LAUNCHER="f:\devel\Slicer4-win64rel\Slicer-build\Slicer.exe" 
-set SLICER_LAUNCHER="c:\Program Files\Slicer 4.2.0-2012-11-02\Slicer.exe" 
-
-%SLICER_LAUNCHER% --python-code "import SlicerRtSliceletTest; slicelet=SlicerRtSliceletTestSlicelet();" --no-splash --no-main-window --additional-module-paths "."
